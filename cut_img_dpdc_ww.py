5cf2c6e6365bb0cacac6b4db0462734863e9bb3c
潘伟志 <1165645958@qq.com>
2022-10-11 16:00:35 +0800

    Update cut_img_dpdc_ww.py

--- cut_img_dpdc_ww.py
+++ cut_img_dpdc_ww.py
@@ -8,7 +8,7 @@ from sonic.utils_func import glob_extensions, cv_img_read
 左下电池x1，y1（200, 575） x2，y2（1000， 1175）
 '''
 
-input_path = r'D:\桌面\无为-2D-虚焊'
+input_path = r'D:\桌面\pth'
 output_path = r'D:\桌面\img'
 img_path_list = glob_extensions(input_path)
 
@@ -22,16 +22,17 @@ for img_path in img_path_list:
     target_index = []
     for i, cnt in enumerate(contours):
         cnt_area = cv2.contourArea(cnt)
-        if 1000000 <= cnt_area <= 1500000:
+        if 1000000 <= cnt_area <= 2000000:
             target_index.append(i)
-    if len(target_index) != 0:
-        x, y, w, h = cv2.boundingRect(contours[target_index[0]])
-        test_img = img[y:y + h, x:x + w].copy()
-        test_h, test_w = test_img.shape[:2]
 
     for index in target_index:
         x, y, w, h = cv2.boundingRect(contours[index])
+        rect = cv2.minAreaRect(contours[index])
         crop_img = img[y:y + h, x:x + w].copy()
+        left_mean = crop_img[150:300, 0:w//2]
+        right_mean = crop_img[150:300, w//2:w]
+        if right_mean < left_mean:
+            crop_img = cv2.flip(crop_img, 1)
         h_crop, w_crop = crop_img.shape[:2]
         if w_crop >= 1740 and h_crop >= 1175:
             crop_up_img = crop_img[10:610, 940:1740].copy()
@@ -39,9 +40,12 @@ for img_path in img_path_list:
             output_img_path = Path(
                 output_path,
                 Path(img_path).relative_to(Path(input_path)))
-            output_img_path = output_img_path.rsplit('.', 1)
+            output_img_path_parent = output_img_path.parent
+            output_img_path_all_name = output_img_path.name
+            output_img_path_name = output_img_path_all_name.split('.')[0]
+
             cv2.imencode(
-                suffix, crop_up_img)[1].tofile(f'{output_img_path}_A{suffix}')
+                suffix, crop_up_img)[1].tofile(f'{output_img_path_parent}/{output_img_path_name}_A{suffix}')
             cv2.imencode(
                 suffix,
-                crop_down_img)[1].tofile(f'{output_img_path}_B{suffix}')
+                crop_down_img)[1].tofile(f'{output_img_path_parent}/{output_img_path_name}_B{suffix}')
