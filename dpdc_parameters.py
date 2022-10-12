4feb0255b3c6dc4d9c8f618917c386b6addb730b
潘伟志 <1165645958@qq.com>
2022-10-12 16:24:35 +0800

    Update dpdc_parameters.py

--- dpdc_parameters.py
+++ dpdc_parameters.py
@@ -1,33 +1,42 @@
+"""
+5号负方向流水线需顺时针旋转2度
+"""
+
 line_5 = {
-    'threshold': [],
+    'threshold_A': [],
+    'threshold_B': [],
     'negative_direction_A': [],
     'negative_direction_B': [],
     'positive_direction_A': [],
     'positive_direction_B': []
 }
 line_6 = {
-    'threshold': [],
+    'threshold_A': [],
+    'threshold_B': [],
     'negative_direction_A': [],
     'negative_direction_B': [],
     'positive_direction_A': [],
     'positive_direction_B': []
 }
 line_7 = {
-    'threshold': [],
+    'threshold_A': [],
+    'threshold_B': [],
     'negative_direction_A': [],
     'negative_direction_B': [],
     'positive_direction_A': [],
     'positive_direction_B': []
 }
 line_8 = {
-    'threshold': [],
+    'threshold_A': [],
+    'threshold_B': [],
     'negative_direction_A': [],
     'negative_direction_B': [],
     'positive_direction_A': [],
     'positive_direction_B': []
 }
 line_9 = {
-    'threshold': [],
+    'threshold_A': [],
+    'threshold_B': [],
     'negative_direction_A': [],
     'negative_direction_B': [],
     'positive_direction_A': [],
