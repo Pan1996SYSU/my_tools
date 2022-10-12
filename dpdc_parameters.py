15744f96dc9e15afd58710494ee4ed500dfc6b94
潘伟志 <1165645958@qq.com>
2022-10-12 15:42:47 +0800

    Update dpdc_parameters.py

--- dpdc_parameters.py
+++ dpdc_parameters.py
@@ -1,25 +1,35 @@
 line_5 = {
     'threshold': [],
     'negative_direction_A': [],
-    'negative_direction_B': []
+    'negative_direction_B': [],
+    'positive_direction_A': [],
+    'positive_direction_B': []
 }
 line_6 = {
     'threshold': [],
     'negative_direction_A': [],
-    'negative_direction_B': []
+    'negative_direction_B': [],
+    'positive_direction_A': [],
+    'positive_direction_B': []
 }
 line_7 = {
     'threshold': [],
     'negative_direction_A': [],
-    'negative_direction_B': []
+    'negative_direction_B': [],
+    'positive_direction_A': [],
+    'positive_direction_B': []
 }
 line_8 = {
     'threshold': [],
     'negative_direction_A': [],
-    'negative_direction_B': []
+    'negative_direction_B': [],
+    'positive_direction_A': [],
+    'positive_direction_B': []
 }
 line_9 = {
     'threshold': [],
     'negative_direction_A': [],
-    'negative_direction_B': []
+    'negative_direction_B': [],
+    'positive_direction_A': [],
+    'positive_direction_B': []
 }
