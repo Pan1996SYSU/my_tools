770bf1216be1af84ce9571b8c61770cb17726032
潘伟志 <1165645958@qq.com>
2022-10-12 13:51:28 +0800

    Update cut_img_dpdc_ww.py

--- cut_img_dpdc_ww.py
+++ cut_img_dpdc_ww.py
@@ -3,12 +3,18 @@ from pathlib import Path
 import cv2
 from sonic.utils_func import glob_extensions, cv_img_read, show_img
 '''
+行列式负方向
 600*800
 右上电池x1，y1（940, 10） x2，y2（1740， 610）
 左下电池x1，y1（200, 575） x2，y2（1000， 1175）
+
+行列式正方向
+600*800
+左上电池x1，y1（110, 30） x2，y2（910， 630）
+右下电池x1，y1（875, 625） x2，y2（1675， 1225）
 '''
 
-input_path = r'D:\桌面\无为-2D-虚焊'
+input_path = r'D:\桌面\pth'
 output_path = r'D:\桌面\img'
 img_path_list = glob_extensions(input_path)
 
@@ -28,26 +34,25 @@ for img_path in img_path_list:
 
     for index in target_index:
         x, y, w, h = cv2.boundingRect(contours[index])
-        # cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 255), 10)
         rect = cv2.minAreaRect(contours[index])
         crop_img = img[y:y + h, x:x + w].copy()
-        # if h >= 300:
-        #     left_mean = crop_img[150:300, 0:w // 2].mean()
-        #     right_mean = crop_img[150:300, w // 2:w].mean()
-        #     if right_mean < left_mean:
-        #         crop_img = cv2.flip(crop_img, 1)
-        #     h_crop, w_crop = crop_img.shape[:2]
-        # if w_crop >= 1740 and h_crop >= 1175:
-        #     crop_up_img = crop_img[10:610, 940:1740].copy()
-        #     crop_down_img = crop_img[575:1175, 200:1000].copy()
-        #     output_img_path = Path(
-        #         output_path,
-        #         Path(img_path).relative_to(Path(input_path)))
-        #     output_img_path_parent = output_img_path.parent
-        #     output_img_path_all_name = output_img_path.name
-        #     output_img_path_name = output_img_path_all_name.split('.')[0]
-        #
-        #     cv2.imencode(suffix, crop_up_img)[1].tofile(
-        #         f'{output_img_path_parent}/{output_img_path_name}_{index}_A{suffix}')
-        #     cv2.imencode(suffix, crop_down_img)[1].tofile(
-        #         f'{output_img_path_parent}/{output_img_path_name}_{index}_B{suffix}')
+        if h >= 300:
+            left_mean = crop_img[150:300, 0:w // 2].mean()
+            right_mean = crop_img[150:300, w // 2:w].mean()
+            if right_mean < left_mean:
+                crop_img = cv2.flip(crop_img, 1)
+            h_crop, w_crop = crop_img.shape[:2]
+        if w_crop >= 1740 and h_crop >= 1175:
+            crop_up_img = crop_img[10:610, 940:1740].copy()
+            crop_down_img = crop_img[575:1175, 200:1000].copy()
+            output_img_path = Path(
+                output_path,
+                Path(img_path).relative_to(Path(input_path)))
+            output_img_path_parent = output_img_path.parent
+            output_img_path_all_name = output_img_path.name
+            output_img_path_name = output_img_path_all_name.split('.')[0]
+
+            cv2.imencode(suffix, crop_up_img)[1].tofile(
+                f'{output_img_path_parent}/{output_img_path_name}_{index}_A{suffix}')
+            cv2.imencode(suffix, crop_down_img)[1].tofile(
+                f'{output_img_path_parent}/{output_img_path_name}_{index}_B{suffix}')
