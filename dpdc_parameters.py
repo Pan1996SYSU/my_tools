926548281344f1cde3796b009f99bb496e367fc7
潘伟志 <1165645958@qq.com>
2022-10-12 17:45:56 +0800

    Update dpdc_parameters.py

--- dpdc_parameters.py
+++ dpdc_parameters.py
@@ -3,40 +3,40 @@
 """
 
 line_5 = {
-    'threshold_A': [],
-    'threshold_B': [],
+    'threshold_negative': [78, 255],
+    'threshold_positive': [119, 255],
     'negative_direction_A': [],
     'negative_direction_B': [],
     'positive_direction_A': [],
     'positive_direction_B': []
 }
 line_6 = {
-    'threshold_A': [],
-    'threshold_B': [],
+    'threshold_negative': [],
+    'threshold_positive': [],
     'negative_direction_A': [],
     'negative_direction_B': [],
     'positive_direction_A': [],
     'positive_direction_B': []
 }
 line_7 = {
-    'threshold_A': [],
-    'threshold_B': [],
+    'threshold_negative': [],
+    'threshold_positive': [],
     'negative_direction_A': [],
     'negative_direction_B': [],
     'positive_direction_A': [],
     'positive_direction_B': []
 }
 line_8 = {
-    'threshold_A': [],
-    'threshold_B': [],
+    'threshold_negative': [],
+    'threshold_positive': [],
     'negative_direction_A': [],
     'negative_direction_B': [],
     'positive_direction_A': [],
     'positive_direction_B': []
 }
 line_9 = {
-    'threshold_A': [],
-    'threshold_B': [],
+    'threshold_negative': [],
+    'threshold_positive': [],
     'negative_direction_A': [],
     'negative_direction_B': [],
     'positive_direction_A': [],
