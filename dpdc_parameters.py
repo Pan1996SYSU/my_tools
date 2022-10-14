715bca458850ede30e5bffaef2a343373f9743a3
潘伟志 <1165645958@qq.com>
2022-10-14 17:11:41 +0800

    Update dpdc_parameters.py

--- dpdc_parameters.py
+++ dpdc_parameters.py
@@ -1,20 +1,23 @@
+'''
+y, x, r
+'''
 line_5 = {
-    'template_img_left_region': [],
-    'template_img_right_region': [],
+    'template_img_left_region': [1301.84, 565.435, 91.8908],
+    'template_img_right_region': [752.368, 1671.85, 85.6912],
 }
 line_6 = {
-    'template_img_left_region': [],
-    'template_img_right_region': [],
+    'template_img_left_region': [1023.7, 490.851, 92.9081],
+    'template_img_right_region': [1007.22, 1203.41, 90.8391],
 }
 line_7 = {
-    'template_img_left_region': [],
-    'template_img_right_region': [],
+    'template_img_left_region': [999.128, 735.744, 86.5653],
+    'template_img_right_region': [1414.03, 1704.59, 88.3937],
 }
 line_8 = {
-    'template_img_left_region': [],
-    'template_img_right_region': [],
+    'template_img_left_region': [496.069, 1234.58, 94.5105],
+    'template_img_right_region': [851.475, 1180.71, 92.7134],
 }
 line_9 = {
-    'template_img_left_region': [],
-    'template_img_right_region': [],
+    'template_img_left_region': [440.478, 1351.74, 86.4068],
+    'template_img_right_region': [805.284, 1089.41, 92.7801],
 }
