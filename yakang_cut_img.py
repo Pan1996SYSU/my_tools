01ad40e0a96aeac09f4dceafdc3a2402a0744892
潘伟志 <1165645958@qq.com>
2022-10-14 16:42:04 +0800

    Update yakang_cut_img.py

--- yakang_cut_img.py
+++ yakang_cut_img.py
@@ -6,49 +6,42 @@ import cv2
 import halcon as ha
 from sonic.utils_func import glob_extensions, cv_img_read
 
-input_path = r"Z:\2-现场取图\CYS.210711-雅康卷绕一体机-麻点寻边\20220912"
+input_path = r"D:\桌面\20221014"
+template_path = r'D:/桌面/20221014/1/10_14_12_00_27_844.676_NG.bmp'
 output_path = r"D:\桌面\img"
 output_path = Path(output_path)
 img_path_list = glob_extensions(input_path)
 
-edge = 150
+T_Image = ha.read_image(template_path)
+ROI = ha.gen_rectangle1(1058.12, 1861.42, 1173.62, 1959.52)
+ImageReduced = ha.reduce_domain(T_Image, ROI)
+ModelImages, ModelRegions = ha.inspect_shape_model(ImageReduced, 1, 30)
+Edges = ha.edges_sub_pix(ImageReduced, 'canny', 1.2, 60, 90)
+ModelID = ha.create_shape_model_xld(
+    Edges, 'auto', 0, 360, 'auto', 'auto', 'ignore_local_polarity', 5)
+
+y_edge = 200
+x_edge = 250
 num = len(img_path_list)
+n = num // 20
+
 for j, img_path in enumerate(img_path_list):
-    if j % 10 == 0:
+    if j % num == 0:
         print(f'{j / num * 100}%')
     try:
         Image = ha.read_image(img_path)
-        Red, Green, Blue = ha.decompose3(Image)
-        Regions = ha.threshold(Green, 226, 255)
-        ImageReduced = ha.reduce_domain(Image, Regions)
-        ImageReducedChannel3 = ha.access_channel(ImageReduced, 3)
-        Regions1 = ha.threshold(ImageReducedChannel3, 138, 255)
-        RegionOpening = ha.opening_rectangle1(Regions1, 2, 2)
-        RegionClosing1 = ha.closing_rectangle1(RegionOpening, 2, 2)
-        RegionClosing = ha.closing_rectangle1(RegionClosing1, 300, 50)
-        RegionOpening1 = ha.opening_rectangle1(RegionClosing, 100, 100)
-        RegionDifference = ha.difference(RegionClosing, RegionOpening1)
-        ConnectedRegions = ha.connection(RegionDifference)
-        SelectedRegions = ha.select_shape(
-            ConnectedRegions, 'area', 'and', 8500, 25000)
-        SelectedRegions1 = ha.select_shape(
-            SelectedRegions, 'inner_height', 'and', 50, 90)
-        SelectedRegions2 = ha.select_shape(
-            SelectedRegions1, 'inner_width', 'and', 120, 300)
-        SelectedRegions3 = ha.select_shape(
-            SelectedRegions2, 'rectangularity', 'and', 0.9, 1)
-        Row, Column, Phi, Length = ha.smallest_rectangle1(SelectedRegions3)
+        Row, Column, Angle, Scale, Score1, Model = ha.find_scaled_shape_models(
+            Image, ModelID, 0, 360, 0.97, 1.03, 0.7, 1, 0.5, 'least_squares',
+            0, 0.9)
         img_path = Path(img_path)
         img = cv_img_read(img_path)
         h, w = img.shape[:2]
         for i in range(len(Row)):
             try:
-                y1 = Row[i]
-                x1 = Column[i]
-                y2 = Phi[i]
-                x2 = Length[i]
-                crop_img = img[max(0, y1 - edge):min(h, y2 + edge),
-                               max(0, x1 - edge):min(w, x2 + edge)].copy()
+                y = int(Row[0])
+                x = int(Column[0])
+                crop_img = img[max(0, y - y_edge):min(h, y + y_edge),
+                               max(0, x - x_edge):min(w, x + x_edge)].copy()
                 img_output_path = Path(
                     output_path, img_path.relative_to(input_path))
                 if not os.path.exists(img_output_path.parent):
@@ -63,3 +56,4 @@ for j, img_path in enumerate(img_path_list):
     except Exception as e:
         print(e)
         print(traceback.format_exc())
+print(f'{num / num * 100}%')
