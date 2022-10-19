34f977bf1794e585282019b5de40b3ef1cc5fcfc
潘伟志 <1165645958@qq.com>
2022-10-19 11:13:25 +0800

    Update cut_tencent_img.py

--- cut_tencent_img.py
+++ cut_tencent_img.py
@@ -56,53 +56,62 @@ def cut_json(json_data, x1, x2):
 
 
 def crop_img(task):
-    file_key = task.get('file_list', None)
-    if file_key is None:
-        return
-    file_list = file_dict[file_key]
-    file_len = len(file_list)
-    if file_len == 4:
-        json_list = []
-    elif file_len == 5:
-        json_list = file_list[0]
-        del file_list[0]
-    else:
-        print('file_list长度不为4或5')
-        print(file_list)
-        return
-    img_list = file_list
-    base_img_path = img_list[2]
-    Image = ha.read_image(base_img_path)
-    Regions = ha.threshold(Image, 149, 255)
-    ConnectedRegions = ha.connection(Regions)
-    SelectedRegions = ha.select_shape(
-        ConnectedRegions, 'area', 'and', 8000000, 16000000)
-    y1, x1, y2, x2 = ha.smallest_rectangle1(SelectedRegions)
-    if len(y1) != 1:
-        print('阈值分割失败，将使用默认坐标')
-        print(img_list)
-        x1 = [1760]
-        x2 = [6200]
-    for img_path in img_list:
-        img = cv_img_read(img_path)
-        h, w = img.shape[:2]
-        res_img = img[0:h, max(0, int(x1[0] - padding)):min(w, int(x2[0] + padding))].copy()
-        output_img_path = Path(
-            output_path,
-            Path(img_path).relative_to(Path(input_path)))
-        suffix = Path(img_path).suffix
-        output_parent = Path(output_img_path).parent
-        make_dirs(output_parent)
-        cv2.imencode(suffix, res_img)[1].tofile(output_img_path)
-    if json_list:
-        json_data = load_json(json_list)
-        json_res_data = cut_json(json_data, x1[0] - padding, x2[0] + padding)
-        output_json_path = Path(
-            output_path,
-            Path(json_list).relative_to(Path(input_path)))
-        output_parent = Path(output_json_path).parent
-        make_dirs(output_parent)
-        save_json(output_json_path, json_res_data)
+    try:
+        file_key = task.get('file_list', None)
+        if file_key is None:
+            return
+        file_list = file_dict[file_key]
+        file_len = len(file_list)
+        if file_len == 4:
+            json_list = []
+        elif file_len == 5:
+            json_list = file_list[0]
+            del file_list[0]
+        else:
+            print('file_list长度不为4或5')
+            print(file_list)
+            return
+        img_list = file_list
+        base_img_path = img_list[2]
+        Image = ha.read_image(base_img_path)
+        Regions = ha.threshold(Image, 149, 255)
+        ConnectedRegions = ha.connection(Regions)
+        SelectedRegions = ha.select_shape(
+            ConnectedRegions, 'area', 'and', 8000000, 16000000)
+        y1, x1, y2, x2 = ha.smallest_rectangle1(SelectedRegions)
+        if len(y1) != 1:
+            print('阈值分割失败，将使用默认坐标')
+            print(img_list)
+            x1 = [1760]
+            x2 = [6200]
+        for img_path in img_list:
+            try:
+                img = cv_img_read(img_path)
+                h, w = img.shape[:2]
+                res_img = img[0:h, max(0, int(x1[0] - padding)):min(w, int(x2[0] + padding))].copy()
+                output_img_path = Path(
+                    output_path,
+                    Path(img_path).relative_to(Path(input_path)))
+                suffix = Path(img_path).suffix
+                output_parent = Path(output_img_path).parent
+                make_dirs(output_parent)
+                cv2.imencode(suffix, res_img)[1].tofile(output_img_path)
+            except Exception as e:
+                print(e)
+                print(traceback.format_exc())
+                print(img_path)
+        if json_list:
+            json_data = load_json(json_list)
+            json_res_data = cut_json(json_data, x1[0] - padding, x2[0] + padding)
+            output_json_path = Path(
+                output_path,
+                Path(json_list).relative_to(Path(input_path)))
+            output_parent = Path(output_json_path).parent
+            make_dirs(output_parent)
+            save_json(output_json_path, json_res_data)
+    except Exception as e:
+        print(e)
+        print(traceback.format_exc())
 
 
 
