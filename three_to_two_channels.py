5233a5e940d7c452720f4ecaed1440df4b8a380f
潘伟志 <1165645958@qq.com>
2022-10-19 15:49:28 +0800

    yapf

--- three_to_two_channels.py
+++ three_to_two_channels.py
@@ -1,7 +1,8 @@
+from pathlib import Path
+
 import cv2
 from sonic.utils_func import cv_img_read
-from sonic.lib.get_img_size import get_image_metadata
-from pathlib import Path
+
 input_path = r'Z:\5-标注数据\20220922-中航叠片电芯-大面\20220925-陈国栋-已标注-检查中\S0000076\05366530767145917440_S000076_C01_P01_L0_PL1_G1_M1_20224525100947_1.bmp'
 output_path = r'D:\桌面\img'
 
