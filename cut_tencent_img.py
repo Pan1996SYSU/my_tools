de9dc9f5a31e6c76905aa1a9144c7716fb551019
潘伟志 <1165645958@qq.com>
2022-10-19 09:29:01 +0800

    Update cut_tencent_img.py

--- cut_tencent_img.py
+++ cut_tencent_img.py
@@ -1,4 +1,5 @@
 import os
+import traceback
 from pathlib import Path
 
 input_path = r'Z:\4-标注任务\20220922-中航叠片电芯-大面'
@@ -13,16 +14,20 @@ def get_files_from_dir(path):
     if not os.path.exists(path):
         return ''
 
-    file_paths = []
-
+    file_path_dict = {}
     for root, directories, files in os.walk(path):
         for filename in files:
             s = Path(filename).suffix
             if s in extensions:
-                filepath = os.path.join(root, filename)
-                file_paths.append(filepath)
-
-    return file_paths
+                try:
+                    name_key = str(filename).split('_C01_P01')[0]
+                    filepath = os.path.join(root, filename)
+                    file_path_dict[name_key] = filepath
+                except Exception as e:
+                    print(e)
+                    print(traceback.format_exc())
+                    print(filename)
+    return file_path_dict
 
 
 a = get_files_from_dir(r'D:\桌面\sth')
