aff0d94826342458709f3bceaf759f546e68ab94
潘伟志 <1165645958@qq.com>
2022-10-25 16:17:55 +0800

    Update tencent_rename.py

--- tencent_rename.py
+++ tencent_rename.py
@@ -4,7 +4,7 @@ from collections import defaultdict
 from glob import iglob
 from pathlib import Path
 
-target_img_path = r'Z:\4-标注任务\CYS.220661-中航裸电芯（2合1）\20221011_传图\大面'
+target_img_path = r'Z:\4-标注任务\CYS.220661-中航裸电芯（2合1）\20221011_传图\大面\20220914_2-39NG-6OK'
 copy_json_path = r'Z:\5-标注数据\CYS.220661-中航裸电芯-大面'
 
 img_and_json_path_dict = defaultdict(set)
@@ -13,6 +13,7 @@ img_path_list = iglob(f'{target_img_path}/**/*', recursive=True)
 for img_path in img_path_list:
     img_name = Path(img_path).name
     common_name = img_name.split('_')[0]
+    common_name = common_name.split('S')[-1]
     img_and_json_path_dict[common_name].add(img_path)
 
 json_path_list = iglob(f'{copy_json_path}/**/*.json', recursive=True)
@@ -20,6 +21,7 @@ for json_path in json_path_list:
     json_name = Path(json_path).name
     common_name = json_name.split('.')[0]
     common_name = common_name.split('_')[0]
+    common_name = common_name.split('S')[-1]
     try:
         img_path_list = img_and_json_path_dict[common_name]
         if len(img_path_list) == 0:
