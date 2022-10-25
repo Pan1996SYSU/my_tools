3aeba7cecd0c7d60add34d15c3eacb4d8f3367cb
潘伟志 <1165645958@qq.com>
2022-10-25 15:57:15 +0800

    Update tencent_rename.py

--- tencent_rename.py
+++ tencent_rename.py
@@ -1,8 +1,10 @@
+import shutil
+import traceback
 from collections import defaultdict
 from glob import iglob
 from pathlib import Path
 
-target_img_path = r'Z:\4-标注任务\CYS.220661-中航裸电芯（2合1）\20221011_传图\侧面'
+target_img_path = r'Z:\4-标注任务\CYS.220661-中航裸电芯（2合1）\20221011_传图\大面'
 copy_json_path = r'Z:\5-标注数据\CYS.220661-中航裸电芯-侧面'
 
 img_and_json_path_dict = defaultdict(set)
@@ -20,11 +22,15 @@ for json_path in json_path_list:
     common_name = common_name.split('_')[0]
     try:
         img_path_list = img_and_json_path_dict[common_name]
-        if len(common_name) == 0:
+        if len(img_path_list) == 0:
             print(f'找不到对应图片{json_path}')
         else:
-            img_first_path = img_path_list[0]
+            img_first_path = list(img_path_list)[0]
             img_path_parent = Path(img_first_path).parent
-
+            json_new_name = str(Path(img_first_path).name).split('_L')[0]
+            json_output_path = f'{img_path_parent}/{json_new_name}.json'
+            new_file = shutil.copy(json_path, json_output_path)
+            print(new_file)
     except:
+        print(traceback.format_exc())
         print(json_path)
