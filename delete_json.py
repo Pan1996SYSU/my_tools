4ed899aa53e7753e199fbcff7c61542d000b758a
潘伟志 <1165645958@qq.com>
2022-10-25 17:20:52 +0800

    Update delete_json.py

--- delete_json.py
+++ delete_json.py
@@ -6,7 +6,7 @@ target_path = r'Z:\4-标注任务\CYS.220661-中航裸电芯（2合1）'
 json_path_list = glob_extensions(target_path, ['.json'])
 n = len(json_path_list)
 
-for i, json_path in enumerate(json_path_list):
-    if i % 20 == 0:
-        print(i / n * 100)
-    os.remove(Path(json_path))
+# for i, json_path in enumerate(json_path_list):
+#     if i % 20 == 0:
+#         print(i / n * 100)
+#     os.remove(Path(json_path))
