5723492189e3eb5492c7fe309f0e5ca942d7fa71
潘伟志 <1165645958@qq.com>
2022-11-05 11:16:42 +0800

    1

--- delete_json.py
+++ delete_json.py
@@ -2,11 +2,11 @@ import os
 from sonic.utils_func import glob_extensions
 from pathlib import Path
 
-target_path = r'Z:\7-标注数据-归档\CYS.220661-中航裸电芯-腾讯定制版\20221011_传图'
+target_path = r'Z:\4-标注任务\CYS.220661-中航裸电芯（2合1）\侧面\问题'
 json_path_list = glob_extensions(target_path, ['.json'])
 n = len(json_path_list)
 
-# for i, json_path in enumerate(json_path_list):
-#     if i % 20 == 0:
-#         print(i / n * 100)
-#     os.remove(Path(json_path))
+for i, json_path in enumerate(json_path_list):
+    if i % 20 == 0:
+        print(i / n * 100)
+    os.remove(Path(json_path))
