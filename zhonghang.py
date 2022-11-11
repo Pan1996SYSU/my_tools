84e0ac5bb0db62f4688d8f4b34f224ba44928700
潘伟志 <1165645958@qq.com>
2022-11-11 15:09:02 +0800

    Update zhonghang.py

--- zhonghang.py
+++ zhonghang.py
@@ -1,10 +1,9 @@
+import matplotlib.pyplot as plt
 import pandas as pd
-
 from pylab import mpl
+
 # 设置显示中文字体
 mpl.rcParams["font.sans-serif"] = ["SimHei"]
-import matplotlib.pyplot as plt
-
 
 file_path = r"D:\桌面\机尾\机尾\A面检测数据2.xls"
 excel_data = pd.read_excel(file_path, header=0)
@@ -16,7 +15,8 @@ for i in range(5, 14):
     y = list(excel_data[head[i]])
 
     index_list = [2475, 2476, 2477, 2478]
-    index_list.reverse()  # 对索引进行反转，使其从后往前删除
+    # 对索引进行反转，使其从后往前删除
+    index_list.reverse()
     for j in index_list:
         if i == 5:
             x.pop(j)
