b21a052aa3a15410f51f0b5e787487e3c0ec8d19
潘伟志 <1165645958@qq.com>
2022-11-22 14:21:20 +0800

    Update move_file.py

--- move_file.py
+++ move_file.py
@@ -2,6 +2,7 @@ from glob import glob
 import os
 from pathlib import Path
 import shutil
+from sonic.utils_func import make_dirs
 
 input_path = r'Z:\Queenie\瑞浦顶盖焊-缺陷检测'
 output_path = r'D:\桌面\img'
@@ -10,5 +11,5 @@ file_path_list = glob(f'{input_path}/**', recursive=True)
 for file_path in file_path_list:
     if os.path.isdir(file_path):
         continue
-    final_output_path = Path(output_path, Path(file_path).relative_to(Path(input_path)))
+    final_output_path = Path(output_path, Path(file_path).name)
     shutil.move(file_path, final_output_path)
