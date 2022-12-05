2babc7f4e4424aa114c328b2f66598c01a352d71
潘伟志 <1165645958@qq.com>
2022-12-05 16:05:15 +0800

    1

--- test.py
+++ test.py
@@ -1,4 +1,6 @@
-from sonic.utils_func import glob_extensions
+from sonic.utils_func import cv_img_read, show_img
+import cv2
+import numpy as np
 
-l = glob_extensions(r"D:\桌面\img")
-print(len(l))
+cv_img = cv2.imdecode(np.fromfile(r"D:\桌面\5.png", dtype=np.uint8), cv2.IMREAD_COLOR)
+show_img(cv_img)
