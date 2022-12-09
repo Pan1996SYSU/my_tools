e35d9f804b9136111c46ca931610c4b3827d66dd
潘伟志 <1165645958@qq.com>
2022-12-09 16:16:13 +0800

    Update circle_detect.py

--- circle_detect.py
+++ circle_detect.py
@@ -1,29 +1,30 @@
 from glob import glob
 from pathlib import Path
-
 import cv2
 import numpy as np
 from skimage.transform import resize
-from sonic.utils_func import load_json, glob_extensions, extensions
+from sonic.utils_func import load_json, glob_extensions, extensions, show_img
+import copy
+import time
 
 
 def has_white_intersection(img, circle):
     height, width = img.shape[:2]
     circle_img = np.zeros((height, width), dtype="uint8")
     x, y, r = circle
-    cv2.circle(circle_img, (x, y), r - 150, (255, 255, 255), -1)
+    cv2.circle(circle_img, (x, y), r - 100, (255, 255, 255), -1)
     image = cv2.bitwise_and(img, circle_img)
 
     # 如果存在相交的白色像素，则返回真
-    if image.any() or x - r < 0 or x + r > width or y - r < 0 or y + r > height:
+    if image.any():
         return True
     else:
         return False
 
 
-input_path = r"D:\桌面\20221209-091543_密封钉-焊偏_20221208_164500_D_焊偏\NG"
-
-padding = 200
+input_path = r"D:\桌面\20221209-160414_密封钉-焊偏_20221208_164500_D_OK"
+kernel = np.ones((20, 20), np.uint8)
+padding = 300
 
 json_path_list = glob_extensions(input_path, [".json"])
 file_path_dict = {
@@ -43,6 +44,7 @@ for key in file_path_dict:
         continue
     json_data = load_json(file_path_list[0])
     for shape in json_data["result"]:
+        s = time.time()
         if shape["category_name"] != "焊偏":
             continue
         x, y, w, h = shape["bbox"]
@@ -57,35 +59,33 @@ for key in file_path_dict:
             padding,
             cv2.BORDER_CONSTANT,
             value=0)
-        circles = cv2.HoughCircles(
-            image,
-            cv2.HOUGH_GRADIENT,
-            1,
-            100,
-            param1=50,
-            param2=30,
-            minRadius=200,
-            maxRadius=600)
-        # 找到最大内接圆
-        if circles is not None:
-            # 将圆的坐标转换为整数，方便绘制
-            circles = np.round(circles[0, :]).astype("int")
+        ret, binary = cv2.threshold(image, 127, 255, cv2.THRESH_BINARY)
+        c = copy.deepcopy(binary)
+        contours, hierarchy = cv2.findContours(binary,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+        areas = [cv2.contourArea(c) for c in contours]
+        # 求出最大的轮廓的索引
+        max_index = np.argmax(areas)
+        cnt = contours[max_index]
+        (x1, y1), radius = cv2.minEnclosingCircle(cnt)
+        hull = cv2.convexHull(cnt)
+        h, w = binary.shape[:2]
+        a = cv2.drawContours(binary, [hull], 0, (255, 255, 255), -1)
+        a = cv2.resize(a, (w, h))
+        b = cv2.bitwise_not(a)
+        result = cv2.bitwise_or(c, b)
+        d = cv2.bitwise_not(result)
+        kernel = np.ones((10, 10), np.uint8)
+        opening = cv2.morphologyEx(d, cv2.MORPH_OPEN, kernel)
 
-            # 再次筛选，排除中心位于白色区域或超出图片范围的圆
-            circles = [
-                c for c in circles if not has_white_intersection(image, c)
-            ]
-
-            # 找到最大内接圆
-            if circles:
-                max_circle = max(circles, key=lambda x: x[2])
-
-                # 绘制最大内接圆
-                cv2.circle(
-                    image, (max_circle[0], max_circle[1]), max_circle[2],
-                    (255, 255, 255), 4)
-
-        # 显示结果
-        cv2.namedWindow('image', 0)
-        cv2.imshow("image", image)
-        cv2.waitKey(0)
+        contours, _ = cv2.findContours(opening, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        areas = [cv2.contourArea(c) for c in contours]
+        # 求出最大的轮廓的索引
+        max_index = np.argmax(areas)
+        contour = contours[max_index]
+        # 求出轮廓的最小外接圆
+        (x2, y2), radius = cv2.minEnclosingCircle(contour)
+        dis = np.sqrt(np.square(x2-x1) + np.square(y1-y2))
+        print(dis, time.time() - s)
+        # cv2.namedWindow('result', 0)
+        # cv2.imshow('result', image)
+        # cv2.waitKey(0)
