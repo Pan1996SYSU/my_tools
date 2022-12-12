2ac7b5021a8bb075866f8e29776c87856f7a3888
潘伟志 <1165645958@qq.com>
2022-12-12 12:02:22 +0800

    yapf

--- crop_img.py
+++ crop_img.py
@@ -1,5 +1,5 @@
-from sonic.utils_func import cv_img_read,show_img
 import cv2
+from sonic.utils_func import cv_img_read, show_img
 
 # img_path = r"D:\桌面\1_001CE230000001CC90305909_17-28-42.bmp.bmp"
 # img = cv_img_read(img_path)
@@ -11,6 +11,6 @@ img_path = r"D:\桌面\1-001CE230000001CC90305909-20221211-052841-Lum.png"
 img = cv_img_read(img_path)
 img_flip = cv2.flip(img, 1)
 h, w = img.shape[:2]
-img_res = img_flip[60:h-40, 200:w-210].copy()
-print(60,h-40, 200,w-210)
+img_res = img_flip[60:h - 40, 200:w - 210].copy()
+print(60, h - 40, 200, w - 210)
 show_img(img_res)
