347846969331dc266d349e297394acbb30765603
潘伟志 <1165645958@qq.com>
2022-12-30 13:43:00 +0800

    1

--- test.py
+++ test.py
@@ -1,6 +1,13 @@
-from sonic.utils_func import cv_img_read, show_img
-import cv2
-import numpy as np
+# Open the file in binary mode
+with open(r"D:\桌面\sth\aaa\ee35db74ff73478d88黑板7c09c4d9c2ef5a.bmp.png", 'rb') as f:
+    # Read the file data into a bytes object
+    data = f.read()
 
-cv_img = cv2.imdecode(np.fromfile(r"D:\桌面\5.png", dtype=np.uint8), cv2.IMREAD_COLOR)
-show_img(cv_img)
+# Modify the file header as desired
+# For example, change the width and height values
+width, height = (100, 100)  # new width and height
+data = data[:16] + width.to_bytes(4, 'big') + height.to_bytes(4, 'big') + data[24:]
+
+# Write the modified data back to the file
+with open(r"D:\桌面\sth\aaa\ee35db74ff73478d88黑板7c09c4d9c2ef5a.bmp.png", 'wb') as f:
+    f.write(data)
