5693468db746f03389967c91c3a4d3ec63e36439
潘伟志 <1165645958@qq.com>
2022-12-30 10:51:43 +0800

    Update find_img.py

--- find_img.py
+++ find_img.py
@@ -1,3 +1,16 @@
+from sonic.utils_func import glob_extensions
+from pathlib import Path
 
+basic_path = r"Z:\4-标注任务\CYS.220717-欣旺达-密封钉检测-2D\一线\pwz\已确认过杀"
+find_path = r"Z:\4-标注任务\CYS.220717-欣旺达-密封钉检测-2D\一线\pwz\NG"
+output_path = Path(r"Z:\4-标注任务\CYS.220717-欣旺达-密封钉检测-2D\一线\pwz\已匹配")
 
-basic_path = r""
+basic_img_path_list = glob_extensions(basic_path)
+find_img_path_list = glob_extensions(find_path)
+
+b_img_path_list = [str(Path(path).stem) for path in basic_path]
+
+for find_img_path in find_img_path_list:
+    if str(Path(find_img_path).stem) in b_img_path_list:
+        output_img_path = Path(output_path, Path(find_img_path).name)
+        
