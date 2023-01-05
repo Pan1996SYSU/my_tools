8246e3f544ad1465c6a5cd4b87d11dc842ae7f50
潘伟志 <1165645958@qq.com>
2023-01-05 16:13:26 +0800

    Update bgr_to_gray.py

--- bgr_to_gray.py
+++ bgr_to_gray.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 
 from PIL import Image, ImageEnhance
-from sonic.utils_func import glob_extensions
+from sonic.utils_func import glob_extensions, load_json, save_json
 
 input_path = r"Z:\5-标注数据\CYS.220717-欣旺达-密封钉检测\9-彩图-停产-暂停\针孔"
 output_path = r"D:\桌面\pth"
@@ -13,6 +13,8 @@ for img_path in img_path_list:
     img_path = Path(img_path)
     suffix = img_path.suffix
     stem = img_path.stem
+    json_path = img_path.with_suffix('.json')
+    json_data = load_json(json_path)
 
     for i in range(5, 10, 1):
         img = Image.open(img_path)
@@ -21,4 +23,6 @@ for img_path in img_path_list:
         img = enhancer.enhance(brightness)
         img = img.convert("L")
         output_img_path = f'{output_path}/{stem}_{i}{suffix}'
+        output_json_path = Path(output_img_path).with_suffix('.json')
         img.save(output_img_path)
+        save_json(output_json_path, json_data)
