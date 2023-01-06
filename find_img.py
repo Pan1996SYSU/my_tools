0a3e1dd8b5fb008cea7935da71cf64c5ae5829e3
潘伟志 <1165645958@qq.com>
2023-01-06 09:11:36 +0800

    Update find_img.py

--- find_img.py
+++ find_img.py
@@ -1,11 +1,12 @@
+import os
 import shutil
 
 from sonic.utils_func import glob_extensions
 from pathlib import Path
 
-basic_path = r"X:\2-现场取图-3D\CYS.220717-欣旺达-密封钉检测-2D\一线\2D-无需分类起\20230101-晚班2D\已确认过杀"
-find_path = r"X:\2-现场取图-3D\CYS.220717-欣旺达-密封钉检测-2D\一线\2D-无需分类起\20230101-晚班2D\原图\NG"
-output_path = Path(r"X:\2-现场取图-3D\CYS.220717-欣旺达-密封钉检测-2D\一线\2D-无需分类起\20230101-晚班2D\pwz已处理")
+basic_path = r"Z:\5-标注数据\CYS.211117-新能德电芯外观检测机\电芯本体\问题图\裁图不正确"
+find_path = r"Z:\5-标注数据\CYS.211117-新能德电芯外观检测机\归档\原图"
+output_path = Path(r"Z:\5-标注数据\CYS.211117-新能德电芯外观检测机\电芯本体\问题图\pwz已处理")
 
 basic_img_path_list = glob_extensions(basic_path)
 find_img_path_list = glob_extensions(find_path)
@@ -15,6 +16,15 @@ b_img_path_list = [str(Path(path).stem) for path in basic_img_path_list]
 for find_img_path in find_img_path_list:
     if str(Path(find_img_path).stem) in b_img_path_list:
         output_img_path = Path(output_path, Path(find_img_path).name)
+        parent = Path(find_img_path).parent
+        stem = Path(find_img_path).stem
+        name = str(stem)[:-10]
+        json_path = f'{str(parent)}/{name}.json'
+        json_output_path = Path(Path(output_img_path).parent,Path(json_path).name)
+        if os.path.exists(json_path):
+            with open(json_path, 'rb') as f_i:
+                with open(json_output_path, 'wb') as f_o:
+                    shutil.copyfileobj(f_i, f_o, 1024 * 1024)
         with open(find_img_path, 'rb') as f_in:
             with open(output_img_path, 'wb') as f_out:
                 shutil.copyfileobj(f_in, f_out, 1024 * 1024)
