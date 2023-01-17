6f31aaf0bec951fed22c23e2529596b6f16a5845
潘伟志 <1165645958@qq.com>
2023-01-17 17:30:15 +0800

    Update magnifier.py

--- magnifier.py
+++ magnifier.py
@@ -4,6 +4,10 @@ from PyQt5.QtCore import QRect
 from PyQt5.QtGui import QPixmap
 from PyQt5.QtWidgets import QApplication, QLabel, QMainWindow, QFrame
 
+FixedSize = 100
+LineWidth = 1
+AreaSize = 30
+
 
 class AreaMagnification(QMainWindow):
 
@@ -19,38 +23,38 @@ class AreaMagnification(QMainWindow):
         self.setCentralWidget(self.image_label)
 
         self.color_label = QLabel(self)
-        self.color_label.setFixedSize(100, 100)
+        self.color_label.setFixedSize(FixedSize, FixedSize)
         self.color_label.setStyleSheet(
             "QLabel { border: 1px solid blue;  \
                                                 background-color: white; position: absolute; left: 0px; top: 0px;}"
         )
 
         self.horizontal_line = QFrame(self.color_label)
-        self.horizontal_line.setGeometry(QRect(0, 50, 100, 1))
+        self.horizontal_line.setGeometry(QRect(0, int(FixedSize/2), FixedSize, LineWidth))
         self.horizontal_line.setFrameShape(QFrame.HLine)
         self.horizontal_line.setFrameShadow(QFrame.Sunken)
-        self.horizontal_line.setLineWidth(1)
+        self.horizontal_line.setLineWidth(LineWidth)
         self.horizontal_line.setStyleSheet('background-color:blue;')
 
         self.vertical_line = QFrame(self.color_label)
-        self.vertical_line.setGeometry(QRect(50, 0, 1, 100))
+        self.vertical_line.setGeometry(QRect(int(FixedSize/2), 0, LineWidth, FixedSize))
         self.vertical_line.setFrameShape(QFrame.VLine)
         self.vertical_line.setFrameShadow(QFrame.Sunken)
-        self.vertical_line.setLineWidth(1)
+        self.vertical_line.setLineWidth(LineWidth)
         self.vertical_line.setStyleSheet('background-color:blue;')
 
     def show_color(self, event):
         x, y = event.pos().x(), event.pos().y()
-        color_area = self.pixmap.copy(x - 15, y - 15, 30, 30)
-        color_area = color_area.scaled(100, 100)
+        color_area = self.pixmap.copy(int(x - AreaSize/2), int(y - AreaSize/2), AreaSize, AreaSize)
+        color_area = color_area.scaled(FixedSize, FixedSize)
         self.color_label.setPixmap(color_area)
 
-        pos_x = x + 30
-        pos_y = y + 30
-        if pos_x + 100 > self.pixmap.width():
-            pos_x -= 160
-        if pos_y + 100 > self.pixmap.height():
-            pos_y -= 160
+        pos_x = x + AreaSize
+        pos_y = y + AreaSize
+        if pos_x + FixedSize > self.pixmap.width():
+            pos_x -= (FixedSize + 2 * AreaSize)
+        if pos_y + FixedSize > self.pixmap.height():
+            pos_y -= (FixedSize + 2 * AreaSize)
         self.color_label.move(pos_x, pos_y)
 
 
