4526af056f8cb39419fdddbe08b06af5a576baa0
潘伟志 <1165645958@qq.com>
2023-01-30 13:14:19 +0800

    Update split_img.py

--- split_img.py
+++ split_img.py
@@ -3,7 +3,6 @@ from sonic.utils_func import glob_extensions, make_dirs
 import shutil
 
 input_path = r"X:\2-现场取图-3D\CYS.220818-利元亨南京国轩整线-盖板激光焊接-转接片\1线\20230119"
-output_path = r"D:\桌面\img"
 
 img_path_list = glob_extensions(input_path)
 
