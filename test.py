9afa729a30731f57b612d769ede88a68c9df9cce
潘伟志 <1165645958@qq.com>
2023-02-04 15:03:15 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,10 @@
 import copy
 import os.path
 from pathlib import Path
+
+import cv2
 import numpy as np
-from sonic.utils_func import glob_extensions, load_json, cv_img_read
+from sonic.utils_func import glob_extensions, load_json, cv_img_read, make_dirs, save_json
 
 padding = 50
 
@@ -30,7 +32,10 @@ for img_path in img_path_list:
                 x_max = x_points.max()
                 y_max = y_points.max()
 
-                crop_img = crop_img = img[max(0, int(y_min - padding)):min(h, int(y_max + padding)), max(0, int(x_min - padding)):min(w, int(x_max + padding))].copy()
+                crop_img = crop_img = img[
+                    max(0, int(y_min - padding)):min(h, int(y_max + padding)),
+                    max(0, int(x_min - padding)):min(w, int(x_max +
+                                                            padding))].copy()
                 js_data = copy.deepcopy(json_data)
 
                 crop_h, crop_w = crop_img.shape[:2]
@@ -46,7 +51,19 @@ for img_path in img_path_list:
                     points[points[:, 1] <= 1, 1] = 2
                     js_data["shapes"][i]['points'] = points.tolist()
 
+                output_img_path = Path(
+                    output_path,
+                    Path(img_path).relative_to(Path(input_path)))
+                parent = output_img_path.parent
+                stem = output_img_path.stem
+                suffix = output_img_path.suffix
 
+                output_img_path_1 = Path(f'{parent}/{stem}_1{suffix}')
+                make_dirs(output_img_path_1.parent)
+                cv2.imencode(suffix, img)[1].tofile(output_img_path_1)
+
+                json_output_path_1 = output_img_path_1.with_suffix('.json')
+                save_json(json_output_path_1, json_data)
 
             elif shape['label'] == '绿胶-带极耳':
                 points = np.array(shape['points'])
@@ -60,3 +77,36 @@ for img_path in img_path_list:
                 y_min = y_points.min()
                 x_max = w
                 y_max = y_points.max()
+
+                crop_img = crop_img = img[
+                    max(0, int(y_min - padding)):min(h, int(y_max + padding)),
+                    max(0, int(x_min - padding)):min(w, int(x_max +
+                                                            padding))].copy()
+                js_data = copy.deepcopy(json_data)
+
+                crop_h, crop_w = crop_img.shape[:2]
+                js_data["imageHeight"] = crop_h
+                js_data["imageWidth"] = crop_w
+                for i, shape in enumerate(js_data["shapes"]):
+                    points = np.array(shape['points'])
+                    points[:, 0] = points[:, 0] - max(0, int(x_min - padding))
+                    points[:, 1] = points[:, 1] - max(0, int(y_min - padding))
+                    points[points[:, 0] >= crop_w - 1, 0] = crop_w - 2
+                    points[points[:, 0] <= 1, 0] = 2
+                    points[points[:, 1] >= crop_h - 1, 1] = crop_h - 2
+                    points[points[:, 1] <= 1, 1] = 2
+                    js_data["shapes"][i]['points'] = points.tolist()
+
+                output_img_path = Path(
+                    output_path,
+                    Path(img_path).relative_to(Path(input_path)))
+                parent = output_img_path.parent
+                stem = output_img_path.stem
+                suffix = output_img_path.suffix
+
+                output_img_path_1 = Path(f'{parent}/{stem}_2{suffix}')
+                make_dirs(output_img_path_1.parent)
+                cv2.imencode(suffix, img)[1].tofile(output_img_path_1)
+
+                json_output_path_1 = output_img_path_1.with_suffix('.json')
+                save_json(json_output_path_1, json_data)
