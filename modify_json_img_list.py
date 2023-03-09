d4fd3aa9b8387ae95aa348b996ee961dc187a0c1
潘伟志 <1165645958@qq.com>
2023-03-09 11:47:50 +0800

    Update modify_json_img_list.py

--- modify_json_img_list.py
+++ modify_json_img_list.py
@@ -1,17 +1,17 @@
-from sonic.utils_func import glob_extensions,load_json,save_json
-from pathlib import Path
+from sonic.utils_func import glob_extensions, load_json, save_json
 
-input_path = r"Z:\4-标注任务\CYS.210333-刀片电池3D焊检测\无为3期\20230307-152349_202303月\NG\auto_label"
+input_path = r"Z:\5-标注数据\CYS.220901-南京欣旺达包蓝膜检测\2023.03.09"
 json_path_list = glob_extensions(input_path, ['.json'])
 for json_path in json_path_list:
     try:
         data = load_json(json_path)
         img_path_list = data["image_path_list"]
-        if len(img_path_list) == 2:
-            if Path(img_path_list[-1]).stem[-1] in ['H', 'h']:
-                data["image_path_list"] = img_path_list + [img_path_list[0]]
-            else:
-                data["image_path_list"] = [img_path_list[-1]] + img_path_list
+        if len(img_path_list) > 6:
+            # if Path(img_path_list[-1]).stem[-1] in ['H', 'h']:
+            #     data["image_path_list"] = img_path_list + [img_path_list[0]]
+            # else:
+            #     data["image_path_list"] = [img_path_list[-1]] + img_path_list
+            data["image_path_list"] = img_path_list[:6]
             data["channels"] = len(data["image_path_list"])
         save_json(json_path, data)
     except:
