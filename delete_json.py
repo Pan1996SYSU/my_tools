2a42d965362bac40a99f917a94296532c3a27328
潘伟志 <1165645958@qq.com>
2023-03-16 12:02:54 +0800

    Update delete_json.py

--- delete_json.py
+++ delete_json.py
@@ -1,12 +1,14 @@
-import os
-from sonic.utils_func import glob_extensions
-from pathlib import Path
+from sonic.utils_func import glob_extensions, load_json, save_json
 
-target_path = r'Z:\4-标注任务\CYS.220661-中航裸电芯（2合1）\侧面\问题'
+target_path = r'D:\桌面\img'
 json_path_list = glob_extensions(target_path, ['.json'])
-n = len(json_path_list)
 
-for i, json_path in enumerate(json_path_list):
-    if i % 20 == 0:
-        print(i / n * 100)
-    os.remove(Path(json_path))
+for json_path in json_path_list:
+    data = load_json(json_path)
+    for i in range(len(data['shapes']) - 1, -1, -1):
+        shape = data['shapes'][i]
+        points = shape['points']
+        if len(set([p[1] for p in points])) != 1:
+            continue
+        data['shapes'].pop(i)
+    save_json(json_path, data)
