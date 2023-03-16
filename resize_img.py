e038a2a5ce86818438da8722059943ae0dbd1309
潘伟志 <1165645958@qq.com>
2023-03-16 11:50:17 +0800

    Update resize_img.py

--- resize_img.py
+++ resize_img.py
@@ -6,8 +6,10 @@ from sonic.utils_func import cv_img_read, glob_extensions, make_dirs, load_json,
 from pathlib import Path
 import cv2
 
-h = 1707
+h = 1211
 w = 2560
+padding = 245
+
 
 input_path = r'Z:\4-标注任务\CYS.221126外观检测ATL-TAB-22015_BCU\裁图'
 output_path = r'D:\桌面\img'
@@ -26,10 +28,13 @@ for img_path in img_path_list:
         w_scale = float(w/img_w)
 
         res_img = cv2.resize(img, (w, h))
+        img_with_border = cv2.copyMakeBorder(res_img, padding, padding, 0, 0, cv2.BORDER_CONSTANT, value=0)
+
+
         output_img_path = Path(output_path, img_path.relative_to(Path(input_path)))
         suffix = output_img_path.suffix
         make_dirs(output_img_path.parent)
-        cv2.imencode(suffix, res_img)[1].tofile(output_img_path)
+        cv2.imencode(suffix, img_with_border)[1].tofile(output_img_path)
 
         if os.path.exists(json_path):
             js = load_json(json_path)
@@ -38,7 +43,8 @@ for img_path in img_path_list:
             for i, shape in enumerate(js["shapes"]):
                 points = np.array(shape['points'])
                 points[:, 0] = points[:, 0] * w_scale
-                points[:, 1] = points[:, 1] * h_scale
+                points[:, 1] = points[:, 1] * w_scale
+                points[:, 1] = points[:, 1] + padding
                 js["shapes"][i]['points'] = points.tolist()
 
             output_json_path = output_img_path.with_suffix('.json')
