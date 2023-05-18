59712b90d99499d1484e2417cfe426ad04adbc60
潘伟志 <1165645958@qq.com>
2023-05-18 15:22:53 +0800

    Update bbbb.py

--- bbbb.py
+++ bbbb.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-from sonic.utils_func import glob_extensions,extensions
+from sonic.utils_func import glob_extensions, extensions
 
 input_path = r'Z:\4-标注任务\CYS.211117-新能德电芯外观检测机\电芯本体-已检查'
 output_path = r'Z:\5-标注数据\CYS.230110-新能德电芯本体\本体'
@@ -12,5 +12,7 @@ file_path_list = glob_extensions(input_path, extensions)
 
 for file_path in file_path_list:
     file_path = Path(file_path)
-
+    category_name = file_path.parent.name
+    file_stem = file_path.stem
+    
 
