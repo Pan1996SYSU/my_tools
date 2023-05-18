727b9c680c25d3ff6b7c5b82d6d1be4e84cc0b57
潘伟志 <1165645958@qq.com>
2023-05-18 15:19:56 +0800

    Create bbbb.py

---
+++ bbbb.py
@@ -0,0 +1,16 @@
+from pathlib import Path
+
+from sonic.utils_func import glob_extensions,extensions
+
+input_path = r'Z:\4-标注任务\CYS.211117-新能德电芯外观检测机\电芯本体-已检查'
+output_path = r'Z:\5-标注数据\CYS.230110-新能德电芯本体\本体'
+
+extensions = [key for key in extensions.keys()]
+extensions.append('.json')
+
+file_path_list = glob_extensions(input_path, extensions)
+
+for file_path in file_path_list:
+    file_path = Path(file_path)
+
+
