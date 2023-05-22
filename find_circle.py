442666bea2881f41ee7f6ac17d95c8ea946742f9
潘伟志 <1165645958@qq.com>
2023-05-22 15:36:23 +0800

    Update find_circle.py

--- find_circle.py
+++ find_circle.py
@@ -4,15 +4,7 @@ import cv2
 import halcon as ha
 from sonic.utils_func import glob_extensions, cv_img_read, make_dirs
 
-r = 850
-
-Image = ha.read_image('D:/桌面/img/01-OK/23年05月08日-13时53分27秒200_2D1C.bmp')
-ROI = ha.gen_circle(987.57, 1328.72, 843.136)
-ImageReduced = ha.reduce_domain(Image, ROI)
-
-Edges = ha.edges_sub_pix(ImageReduced, 'canny', 1, 40, 70)
-ModelID = ha.create_shape_model_xld(
-    Edges, 'auto', -180, 180, 'auto', 'auto', 'ignore_local_polarity', 1)
+r = 100
 
 input_path = r'D:/桌面/img'
 img_path_list = glob_extensions(input_path)
@@ -21,17 +13,20 @@ output_path = r'D:\桌面\新建文件夹'
 for img_path in img_path_list:
     try:
         Image = ha.read_image(img_path)
-        Width, Height = ha.get_image_size(Image)
-        Row, Column, Angle, Scale, Score1, Model = ha.find_scaled_shape_models(
-            Image, ModelID, -180, 180, 1, 1, 0.5, 1, 0, 'least_squares', 0,
-            0.9)
-        x = round(Column[0])
-        y = round(Row[0])
+        Regions = ha.threshold(Image, 179, 255)
+        ConnectedRegions = ha.connection(Regions)
+        SelectedRegions = ha.select_shape(ConnectedRegions, 'area', 'and', 180000, 200000)
+        Row1, Column1, Row2, Column2 = ha.smallest_rectangle1(SelectedRegions)
+
+        y1 = round(Row1[0]) - r
+        x1 = round(Column1[0]) - r
+        y2 = round(Row2[0]) + r
+        x2 = round(Column2[0]) + r
 
         img = cv_img_read(img_path)
         h, w = img.shape[:2]
-        res_img = img[max(0, y - r):min(h, y + r),
-                      max(0, x - r):min(w, x + r)].copy()
+        res_img = img[max(0, y1):min(h, y2),
+                      max(0, x1):min(w, x2)].copy()
         img_suffix = Path(img_path).suffix
         output = Path(
             output_path,
