40fa81827f5ccc36f814f52d8671348e2d7ef23c
潘伟志 <1165645958@qq.com>
2023-05-23 11:00:28 +0800

    Update find_circle.py

--- find_circle.py
+++ find_circle.py
@@ -6,7 +6,7 @@ from sonic.utils_func import glob_extensions, cv_img_read, make_dirs
 
 r = 100
 
-input_path = r"D:\桌面\img\05-其他"
+input_path = r"D:\桌面\img\03-外壳变形"
 img_path_list = glob_extensions(input_path)
 output_path = r'D:\桌面\新建文件夹'
 
@@ -14,11 +14,11 @@ for img_path in img_path_list:
     try:
         Image = ha.read_image(img_path)
         ImageGauss = ha.gauss_filter(Image, 11)
-        Regions = ha.threshold(ImageGauss, 180, 255)
+        Regions = ha.threshold(ImageGauss, 50, 255)
         ConnectedRegions = ha.connection(Regions)
         SelectedRegions = ha.select_shape(ConnectedRegions, 'area', 'and', 10000, 900000)
-        SelectedRegions1 = ha.select_shape(SelectedRegions, 'roundness', 'and', 0.65, 1.0)
-        SelectedRegions2 = ha.select_shape(SelectedRegions1, 'height', 'and', 400, 1700)
+        SelectedRegions1 = ha.select_shape(SelectedRegions, 'roundness', 'and', 0.1, 1.0)
+        SelectedRegions2 = ha.select_shape(SelectedRegions1, 'height', 'and', 1000, 1700)
         Row1, Column1, Row2, Column2 = ha.smallest_rectangle1(SelectedRegions2)
 
         if not Row1 or not Row2 or not Column1 or not Column2:
