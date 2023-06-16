24a7d7cf054c853b4964eab0917dc0c24a1eee45
潘伟志 <1165645958@qq.com>
2023-06-16 11:40:36 +0800

    1

---
+++ generate_img.py
@@ -0,0 +1,15 @@
+import csv
+import numpy as np
+from sonic.utils_func import show_img
+
+# 读取csv文件
+with open(r"D:\桌面\lstsq\lstsq.csv", 'r') as csvfile:
+    reader = csv.reader(csvfile)
+    rows = [row for row in reader]
+
+# 将字符串转换为浮点数并除以1048576
+data = np.array(rows).astype(np.float32) / 1048576.0
+
+# 将数据转换为8位整数
+data = (data * 255).astype(np.uint8)
+show_img(data)
