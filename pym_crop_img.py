156165926e1cd39c0d47284c5b15dbe3cc1e2775
潘伟志 <1165645958@qq.com>
2023-06-21 16:37:27 +0800

    1

--- pym_crop_img.py
+++ pym_crop_img.py
@@ -4,15 +4,21 @@ import cv2
 import halcon as ha
 from sonic.utils_func import glob_extensions, cv_img_read, make_dirs
 
-# input_path = r'Z:\2-现场取图\CYS.230413-分条机增加外观检测ATL-FTJJC-23023\大图-原图\20230611\新建文件夹'
-# output_path = r'Z:\2-现场取图\CYS.230413-分条机增加外观检测ATL-FTJJC-23023\大图-原图\20230611\新建文件夹-pwz已处理'
+input_path = r'Z:\2-现场取图\CYS.230413-分条机增加外观检测ATL-FTJJC-23023\大图-原图\20230620-验证'
+output_path = r'D:\桌面\img'
 
-padding = 10
+padding = 5
 
 img_path_list = glob_extensions(input_path)
 
-for img_path in img_path_list:
+n = len(img_path_list)
+
+aa = n//100
+
+for i, img_path in enumerate(img_path_list):
     try:
+        if i % aa == 0:
+            print(f'{round(float(i/n), 2)*100}%')
         Image = ha.read_image(img_path)
         ImageMedian = ha.median_image(Image, 'square', 55, 'mirrored')
         Regions = ha.threshold(ImageMedian, 141, 255)
@@ -26,15 +32,15 @@ for img_path in img_path_list:
             h, w = img.shape[:2]
 
             if row1[0] < column1[1]:
-                x1 = round(min(column1[0], column1[1]))
-                y1 = round(row2[0]) - padding
-                x2 = round(max(column2[0], column1[1])) - padding
-                y2 = round(row1[1]) + padding
-            else:
-                x1 = round(min(column1[0], column1[1])) - padding
-                y1 = round(row2[1]) - padding
+                x1 = round(min(column1[0], column1[1])) + padding*3
+                y1 = round(row2[0]) - padding*4
                 x2 = round(max(column2[0], column1[1]))
-                y2 = round(row1[0]) + padding
+                y2 = round(row1[1]) + padding*4
+            else:
+                x1 = round(min(column1[0], column1[1])) + padding*3
+                y1 = round(row2[1]) - padding*4
+                x2 = round(max(column2[0], column1[1]))
+                y2 = round(row1[0]) + padding*4
 
             res = img[max(0, y1):min(h, y2), max(0, x1):min(w, x2)].copy()
             output_img_path = Path(
