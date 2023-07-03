0e40059eacba11a69c23c5e4413b73387f4979cd
潘伟志 <1165645958@qq.com>
2023-07-03 15:31:11 +0800

    Update generate_img.py

--- generate_img.py
+++ generate_img.py
@@ -3,13 +3,8 @@ import numpy as np
 from sonic.utils_func import show_img
 
 # 读取csv文件
-with open(r"D:\桌面\lstsq2.csv", 'r') as csvfile:
+with open(r"D:\桌面\ANSS5.txt", 'r') as csvfile:
     reader = csv.reader(csvfile)
     rows = [row for row in reader]
 
-# 将字符串转换为浮点数并除以1048576
-data = np.array(rows).astype(np.float32)
-
-# 将数据转换为8位整数
-data = (data * 255).astype(np.uint8)
-show_img(data)
+print(123)
