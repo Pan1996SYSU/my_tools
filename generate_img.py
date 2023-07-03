5c28c98e290cfe16e6aff0774c78b560e90a9243
潘伟志 <1165645958@qq.com>
2023-07-03 15:50:53 +0800

    Update generate_img.py

--- generate_img.py
+++ generate_img.py
@@ -9,7 +9,7 @@ with open(r"D:\桌面\ANSS5.txt", 'r') as csvfile:
     rows = [row for row in reader]
 
 data = np.array(rows).astype(np.float32)
-img = rows.reshape((8000, 8192, 3))
+img = data.reshape((8000, 8192, 3))
 normal = cv2.normalize(
     img, None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8U)
 cv2.imencode('.tiff', normal)[1].tofile('D:\桌面\ANSS5.tiff')
