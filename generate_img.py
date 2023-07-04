a080ca4f606e5d0baac3cba7bf5cda39c284c1c8
潘伟志 <1165645958@qq.com>
2023-07-04 09:20:36 +0800

    Update generate_img.py

--- generate_img.py
+++ generate_img.py
@@ -8,9 +8,9 @@ with open(r"D:\桌面\ANSS5.txt", 'r') as csvfile:
     reader = csv.reader(csvfile)
     rows = [row for row in reader]
 
-rows = np.array(rows[:16384000]).astype(np.float32)
+rows = np.array(rows[16384000:16384000*2]).astype(np.float32)
 
-img = rows.reshape((8000, 2048, 3))
+img = rows.reshape((2000, 2048*4, 3))
 normal = cv2.normalize(
     img, None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8U)
-cv2.imencode('.tiff', normal)[1].tofile(r'D:\桌面\ANSS5_pinv1.tiff')
+cv2.imencode('.tiff', normal)[1].tofile(r'D:\桌面\ANSS5_pinv2.tiff')
