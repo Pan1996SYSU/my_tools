2d253dd4988e3041b561561eb691c4020263d953
潘伟志 <1165645958@qq.com>
2023-07-07 13:50:09 +0800

    Create super.py

---
+++ super.py
@@ -0,0 +1,17 @@
+class Parent1:
+    def __init__(self, param1):
+        self.param1 = param1
+        print("Initializing Parent1 with param1 =", self.param1)
+
+class Parent2:
+    def __init__(self, param2):
+        self.param2 = param2
+        print("Initializing Parent2 with param2 =", self.param2)
+
+class Child(Parent1, Parent2):
+    def __init__(self, param1, param2):
+        Parent1.__init__(self, param1)
+        Parent2.__init__(self, param2)
+        print("Initializing Child")
+
+child = Child("value1", "value2")
