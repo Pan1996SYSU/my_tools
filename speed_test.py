73e0589f15c115e6367883c10a28a0534b95549f
潘伟志 <1165645958@qq.com>
2023-07-25 16:38:18 +0800

    Create speed_test.py

---
+++ speed_test.py
@@ -0,0 +1,32 @@
+import os
+from PIL import Image
+from datetime import datetime
+
+
+def read_images_from_path(path):
+    # 获取指定路径下的所有文件名
+    filenames = os.listdir(path)
+
+    for filename in filenames:
+        # 检查文件是否为图片文件
+        if filename.endswith('.jpg') or filename.endswith('.png') or filename.endswith('.jpeg'):
+            # 拼接完整的文件路径
+            image_path = os.path.join(path, filename)
+
+            # 读取图像并计算读取时间
+            start_time = datetime.now()
+            image = Image.open(image_path)
+            end_time = datetime.now()
+            read_time = (end_time - start_time).total_seconds()
+
+            if read_time > 1:
+                # 打印图像的路径和读取时间
+                print(f"图像路径：{image_path}")
+                print(f"读取时间：{read_time}\n")
+
+
+# 指定要读取的图像路径
+image_path = r"Z:\2-现场取图\CYS.230118-卷绕CCD-柳州极耳翻折\20230725-AI图"
+
+# 调用函数读取图像并打印读取时间
+read_images_from_path(image_path)
