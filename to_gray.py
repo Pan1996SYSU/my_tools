ff247dc1bbd9cf9dd0bf2186b4754473356ce276
潘伟志 <1165645958@qq.com>
2023-08-21 16:07:24 +0800

    Update to_gray.py

--- to_gray.py
+++ to_gray.py
@@ -1,13 +1,51 @@
-from sonic.utils_func import glob_extensions, make_dirs
-from pathlib import Path
-import cv2
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QHBoxLayout
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QPalette, QColor
 
-paths = glob_extensions(r"D:\baidu\ikonate_blue")
-for p in paths:
-    img = cv2.imread(p,cv2.IMREAD_UNCHANGED)
-    img[:, :, 3][img[:, :, 3] > 180] = 180
-    p = Path(p)
-    suffix = p.suffix
-    output_img_path = Path(r"D:\桌面\icons", Path(p).relative_to(Path(r"D:\baidu")))
-    make_dirs(output_img_path.parent)
-    cv2.imencode(suffix, img)[1].tofile(output_img_path)
+
+class WidgetOne(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setAutoFillBackground(True)
+        self.setMouseTracking(True)
+        self.setFixedSize(100, 100)
+        self.white_palette = QPalette()
+        self.white_palette.setColor(QPalette.Background, QColor(Qt.white))
+        self.gray_palette = QPalette()
+        self.gray_palette.setColor(QPalette.Background, QColor(Qt.gray))
+
+    def enterEvent(self, event):
+        self.setPalette(self.gray_palette)
+
+    def leaveEvent(self, event):
+        self.setPalette(self.white_palette)
+
+
+class WidgetTwo(QWidget):
+    def __init__(self):
+        super().__init__()
+        layout = QVBoxLayout()
+
+        for _ in range(3):
+            widget_one = WidgetOne()
+            layout.addWidget(widget_one)
+
+        self.setLayout(layout)
+
+
+class MainWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        widget_two = WidgetTwo()
+        main_layout = QVBoxLayout()
+        main_layout.addWidget(widget_two)
+        self.setLayout(main_layout)
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
