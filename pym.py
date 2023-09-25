2d47b5c7ad7618c2bc09907aec4217db287a03d7
潘伟志 <1165645958@qq.com>
2023-09-25 10:24:13 +0800

    Update pym.py

--- pym.py
+++ pym.py
@@ -4,7 +4,7 @@ import cv2
 import halcon as ha
 from sonic.utils_func import glob_extensions, cv2_read_img, make_dirs
 
-img_path = r'Z:\2-现场取图\CYS.230413-分条机增加外观检测ATL-FTJJC-23023\大图-原图\20230904\0904'
+img_path = r'Z:\2-现场取图\CYS.230621-雅策瑞凹版涂布\大图原图\20230918\相机1'
 output_path = r'D:\桌面\img'
 
 img_path_list = glob_extensions(img_path)
@@ -12,27 +12,17 @@ img_path_list = glob_extensions(img_path)
 for path in img_path_list:
     try:
         Image = ha.read_image(path)
-        Regions = ha.threshold(Image, 40, 255)
+        Regions = ha.threshold(Image, 140, 255)
         ConnectedRegions = ha.connection(Regions)
-        SelectedRegions = ha.select_shape(ConnectedRegions, 'area', 'and',
-                                          10000000, 99999999)
+        SelectedRegions = ha.select_shape(ConnectedRegions, 'height', 'and',
+                                          500, 9999)
         row, column, length1, length2 = ha.smallest_rectangle1(SelectedRegions)
-        row2 = [length1[0] + row[0]]
-        column2 = [length2[0] + column[0]]
-        Rectangle = ha.gen_rectangle1(row, column, row2, column2)
-        ReducedImage = ha.reduce_domain(Image, Rectangle)
-        Regions1 = ha.threshold(ReducedImage, 63, 113)
-        ConnectedRegions1 = ha.connection(Regions1)
-        SelectedRegions1 = ha.select_shape(ConnectedRegions1, 'area', 'and',
-                                           10000000, 99999999)
-        Row1, Column1, Row2, Column2 = ha.smallest_rectangle1(SelectedRegions1)
-        y1 = Row1[0]
-        x1 = Column1[0]
-        y2 = Row2[0]
-        x2 = Column2[0]
+        x1 = column[0]
+        x2 = length2[0]
 
         img = cv2_read_img(path)
-        crop_img = img[y1:y2, x1:x2].copy()
+        h, w = img.shape[:2]
+        crop_img = img[:, 0:x2].copy()
 
         path = Path(path)
         output_img_path = Path(output_path, path.relative_to(Path(img_path)))
