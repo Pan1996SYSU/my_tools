76f0f4cf153a5e2470cc35c3eec675d56719df04
潘伟志 <1165645958@qq.com>
2023-09-25 13:44:22 +0800

    Update pym.py

--- pym.py
+++ pym.py
@@ -4,32 +4,28 @@ import cv2
 import halcon as ha
 from sonic.utils_func import glob_extensions, cv2_read_img, make_dirs
 
-img_path = r'Z:\2-现场取图\CYS.230413-分条机增加外观检测ATL-FTJJC-23023\大图-原图\20230904\0904'
+img_path = r'D:\桌面\新建文件夹'
 output_path = r'D:\桌面\img'
-
+padding = 3
 img_path_list = glob_extensions(img_path)
 
 for path in img_path_list:
     try:
         Image = ha.read_image(path)
-        Regions = ha.threshold(Image, 40, 255)
+        Regions = ha.threshold(Image, 140, 255)
         ConnectedRegions = ha.connection(Regions)
-        SelectedRegions = ha.select_shape(ConnectedRegions, 'area', 'and',
-                                          10000000, 99999999)
-        row, column, length1, length2 = ha.smallest_rectangle1(SelectedRegions)
-        row2 = [length1[0] + row[0]]
-        column2 = [length2[0] + column[0]]
-        Rectangle = ha.gen_rectangle1(row, column, row2, column2)
+        SelectedRegions = ha.select_shape(ConnectedRegions, 'width', 'and', 500, 9999)
+        Row1, Column1, Row2, Column2 = ha.smallest_rectangle1(SelectedRegions)
+        Rectangle = ha.gen_rectangle1(Row1, Column1, Row2, Column2)
         ReducedImage = ha.reduce_domain(Image, Rectangle)
-        Regions1 = ha.threshold(ReducedImage, 63, 113)
+        Regions1 = ha.threshold(ReducedImage, 0, 140)
         ConnectedRegions1 = ha.connection(Regions1)
-        SelectedRegions1 = ha.select_shape(ConnectedRegions1, 'area', 'and',
-                                           10000000, 99999999)
+        SelectedRegions1 = ha.select_shape(ConnectedRegions1, 'height', 'and', 500, 9999)
         Row1, Column1, Row2, Column2 = ha.smallest_rectangle1(SelectedRegions1)
-        y1 = Row1[0]
-        x1 = Column1[0]
-        y2 = Row2[0]
-        x2 = Column2[0]
+        y1 = Row1[0] + padding
+        x1 = Column1[0] + padding
+        y2 = Row2[0] - padding
+        x2 = Column2[0] - padding
 
         img = cv2_read_img(path)
         crop_img = img[y1:y2, x1:x2].copy()
