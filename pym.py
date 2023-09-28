a3092562c3bb1f9ddb4d292d0e6f96c122348457
潘伟志 <1165645958@qq.com>
2023-09-28 11:02:52 +0800

    1

--- pym.py
+++ pym.py
@@ -5,7 +5,7 @@ import halcon as ha
 import numpy as np
 from sonic.utils_func import glob_extensions, cv2_read_img, make_dirs
 
-img_path = r'D:\桌面\新建文件夹'
+img_path = r'D:\桌面\20230927'
 output_path = r'D:\桌面\img'
 padding = 5
 img_path_list = glob_extensions(img_path)
@@ -29,7 +29,7 @@ for i, path in enumerate(img_path_list):
         ConnectedRegions1 = ha.connection(Regions1)
         SelectedRegions1 = ha.select_shape(ConnectedRegions1, 'area', 'and', 10000000, 99999999)
         Row1, Column1, Row2, Column2 = ha.smallest_rectangle1(SelectedRegions1)
-        y1 = Row1[0] + padding*2
+        y1 = Row1[0] + padding*3
         x1 = Column1[0] + padding
         y2 = Row2[0] - padding*2
         x2 = Column2[0] - padding
--- pym_crop_img.py
+++ pym_crop_img.py
@@ -2,19 +2,18 @@ from pathlib import Path
 
 import cv2
 import halcon as ha
-import numpy as np
 from sonic.utils_func import glob_extensions, cv2_read_img, make_dirs
 
-input_path = r'D:\桌面\20230926'
+input_path = r'D:\桌面\20230923'
 output_path = r'D:\桌面\img'
 
-padding = 5
+padding = 50
 
 img_path_list = glob_extensions(input_path)
 
 n = len(img_path_list)
 
-aa = n//100
+aa = n // 100
 
 for i, img_path in enumerate(img_path_list):
     try:
@@ -23,21 +22,24 @@ for i, img_path in enumerate(img_path_list):
         Image = ha.read_image(img_path)
         Regions = ha.threshold(Image, 160, 255)
         ConnectedRegions = ha.connection(Regions)
-        SelectedRegions = ha.select_shape(
-            ConnectedRegions, 'height', 'and', 2000, 10000)
+        SelectedRegions = ha.select_shape(ConnectedRegions, 'width', 'and',
+                                          2000, 10000)
         row1, column1, row2, column2 = ha.smallest_rectangle1(SelectedRegions)
-        sorted_list = sorted(column1 + column2)
+        sorted_list = sorted(row1 + row2)
+        sorted_col = sorted(column1 + column2)
         if len(sorted_list) != 4:
             print(img_path)
             continue
-        x1 = sorted_list[1]
-        x2 = sorted_list[2]
+        y1 = sorted_list[1]
+        y2 = sorted_list[2]
+        x1 = sorted_col[1]
+        x2 = sorted_col[2]
         img = cv2_read_img(img_path)
-        result = img[:, x1+padding:x2-padding].copy()
+        result = img[y1 + padding:y2 - padding, x1:x2].copy()
         img_path = Path(img_path)
-        output_img_path = Path(output_path, img_path.relative_to(Path(input_path)))
+        output_img_path = Path(output_path,
+                               img_path.relative_to(Path(input_path)))
         make_dirs(output_img_path.parent)
-        cv2.imencode(output_img_path.suffix,
-                     result)[1].tofile(output_img_path)
+        cv2.imencode(output_img_path.suffix, result)[1].tofile(output_img_path)
     except:
         print(img_path)
